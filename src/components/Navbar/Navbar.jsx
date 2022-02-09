bb6f602cc4dac7a62af493a22f70b75eb03543e3
IG <iikim2511@gmail.com>
2022-02-09 14:11:07 -0500

    S:import img-src

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+
+import images from '../../constants/images.js'
+
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <p>navbar</p>
+
+
+    <div className="app__navbar-logo">
+    <img src={images.G} alt="app__logo" />
+    </div>
+  </div>
+
   )
 }
 
