70759c7f1623175c9d74dc56cc4185cd582849a4
IG <iikim2511@gmail.com>
2022-02-09 18:14:40 -0500

    fin

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -56,24 +56,47 @@ const Navbar = () => {
         
         */}
 
-        { toggleMenu ? 
+        { 
+          toggleMenu 
+          
+          ? 
         
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>          
-          {/*🍀onClick*/} 
-          <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
+            {/*🍀onClick*/} 
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
 
-  
-          <ul className="app__navbar-smallscreen_links">
-            <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
-            <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
-            <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
-            <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
-            <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
-          </ul>      
-        </div>
-        : null
-        
+    
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+            </ul>      
+          </div>
+
+        : null        
         }
+
+
+            {/* 같은 코딩, javascript mastery 방식
+                {toggleMenu &&(
+
+                  <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>          
+              
+                  <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{setToggleMenu(false)}}/>
+
+          
+                  <ul className="app__navbar-smallscreen_links">
+                    <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+                    <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+                    <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+                    <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+                    <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+                  </ul>      
+                </div>
+                )}
+          */} 
 
 
 
