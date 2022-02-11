d8542e1efcfa70aa1b8c98e3f1fa2a3c4f346c6c
IG <iikim2511@gmail.com>
2022-02-11 17:28:38 -0500

    success-video useRef

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,17 +1,80 @@
 import React from 'react'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
 
 import {meal} from '../../constants'
 import './Intro.css'
 
 const Intro = () => {
 
-    const countRef = React.useRef(1);
+    /* 🍀🍄
+        10. useRef
+        20. video : ref={vidRef}
 
-    console.log(countRef);
+        왜인지 모르지만, app__video div에 css,position: relative 넣어야지 play버튼이 보임
+    
+    
+    */
+    const vidRef = React.useRef();
+    const [playVideo,setPlayVideo] = React.useState(false);
+
   return (
     <div className="app__video">
         
-        <video src={meal}/>
+        <video 
+         loop
+         type="video/mp4"
+         controls={false}
+         muted     
+         autoPlay
+        
+         ref={vidRef}
+        src={meal}/>
+
+        <div className="app__video-overlay flex__center">
+            <div 
+                onClick={()=>{
+
+                    /* 
+                        setPlayVideo(!playVideo);
+                        playVideo의 반대로 set
+
+                        playVideo가 true 일때 -> false
+                        playVideo가 false 일때 -> true
+                    
+                    */
+                    setPlayVideo(!playVideo);
+                    
+
+                    /* 
+                    🍀JS Video play() Method                    
+                        playVideo가 true 일때 -> ~.pause()
+                        playVideo가 false 일때 ->  ~.play()                    
+                    */
+
+                    if (playVideo) {
+                        vidRef.current.pause();                                        
+                    } else {
+                        vidRef.current.play();
+                    }                
+                    console.log({playVideo});
+                    console.log(vidRef.current)
+                }}
+                className="app__video-overlay_circle flex__center"
+            >
+
+            {/*   playVideo가 true 일때 -> pause icon
+                        playVideo가 false 일때 -> play icon
+                    
+                    */}
+                {
+                    playVideo?
+                    (<BsPauseFill color="#fff" fontSize={30}/> )
+                    :(<BsFillPlayFill color="#fff" fontSize={30}/> )
+                }            
+            </div>
+        
+        </div>
     
     </div>
   )
