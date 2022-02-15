7f051bca237efb78b5144ae2c54ea26e1184c2fe
IG <iikim2511@gmail.com>
2022-02-15 17:46:11 -0500

    award-fin

--- src/container/Awards/Awards.jsx
+++ src/container/Awards/Awards.jsx
@@ -6,19 +6,32 @@ import { images, data } from '../../constants';
 
 import './Awards.css'
 
+/* 
+🦄🍄js210
 
-// const AwardCard = ({pp_award : {imgUrl, title, subtitle}})=>(
+05. map loop + data , AwardCard
+10. props : const변수에도 전송 ok
+
+20. const함수안에 div JSX 넣을 수 있음
+
+const ~~ = (~)=>(<div>JSX~)
+*/
+
+// 🍉js210-20
 const AwardCard = ({pp_award})=>(
     <div className="app__laurels_awards-card">
+
         <div className='explain'>map loop + data , AwardCard</div>
+
         <img src={pp_award.imgUrl}/>
+
         <div className="app__laurels_awards-card_content">
-        <p className="p__cormorant" style={{ color: '#DCCA87' }}>
-         {pp_award.title}
-        </p>
-        <p className="p__opensans">
-          {pp_award.subtitle}
-        </p>
+            <p className="p__cormorant" style={{ color: '#DCCA87' }}>
+            {pp_award.title}
+            </p>
+            <p className="p__opensans">
+            {pp_award.subtitle}
+            </p>
         </div>
     
     </div>
@@ -36,6 +49,7 @@ const Awards = () => {
                 Our Awards
             </h1>
             
+            {/*🍉js210-05,10 */}
             <div className="app__laurels_awards">
               {
                 data.awards.map(
