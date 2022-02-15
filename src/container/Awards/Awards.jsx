faebeb4476a5068f18b379a85872cb1a1d26803a
IG <iikim2511@gmail.com>
2022-02-15 17:20:16 -0500

    color-change

--- src/container/Awards/Awards.jsx
+++ src/container/Awards/Awards.jsx
@@ -7,20 +7,38 @@ import { images, data } from '../../constants';
 import './Awards.css'
 
 
+// const AwardCard = ({pp_award : {imgUrl, title, subtitle}})=>(
+const AwardCard = ({pp_award})=>(
+    <div className="app__laurels_awards-card">
+        <img src={pp_award.imgUrl}/>
+        <div>
+            <p>{pp_award.title}</p>
+        </div>
+    
+    hello
+    </div>
+)
+
 const Awards = () => {
   return (
     <div className="app__bg app__wrapper section__padding" id="awards">
 
-    <div className="app__wrapper_info">
-        <SubHeading title="Awards & recognition" />
-        <h1 className="headtext__cormorant">
-          Our Awards
-        </h1>
+        <div className="app__wrapper_info">
 
-        
-        </div>
+            <SubHeading title="Awards & recognition" />
 
-    
+            <h1 className="headtext__cormorant">
+                Our Awards
+            </h1>
+            
+            <div className="app__laurels_awards">
+              {
+                data.awards.map(
+                    (p_award)=>(
+                        <AwardCard pp_award={p_award} key={p_award.title}/>))
+              }        
+            </div>    
+        </div>   
     </div>
   )
 }
